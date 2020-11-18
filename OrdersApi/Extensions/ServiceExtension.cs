7357ccfb9f3614a341eb51848b830b0b56e87fe2
philip omoigui <philipomoigui@gmail.com>
2020-11-18 16:37:10 +0100

    Created OrderAPI Microservice to manage order from web client

---
+++ OrdersApi/Extensions/ServiceExtension.cs
@@ -0,0 +1,32 @@
+﻿using GreenPipes;
+using MassTransit;
+using Messaging.InterfacesConstant.Constants;
+using Microsoft.Extensions.DependencyInjection;
+using OrdersApi.Messages.Consumer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersApi.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigureRabbitWithMT(this IServiceCollection services)
+        {
+            services.AddSingleton(provider => Bus.Factory.CreateUsingRabbitMq(
+                config =>
+                {
+                    config.Host("localhost", "/", h => { });
+                    config.ReceiveEndpoint(RabbitMqMassTransitConstants.RegisterOrderCommandQueue, e => {
+                        e.PrefetchCount = 16;
+                        e.UseMessageRetry(x => x.Interval(2, TimeSpan.FromSeconds(10)));
+                        e.Consumer<RegisterOrderCommandConsumer>(provider);
+                    });
+
+                    config.ConfigureEndpoints((IBusRegistrationContext)provider);
+                }
+            ));
+        }
+    }
+}
---
+++ OrdersApi/Messages/Consumer/RegisterOrderCommandConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using Messaging.InterfacesConstant.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrdersApi.Messages.Consumer
+{
+    public class RegisterOrderCommandConsumer : IConsumer<IRegisterOrderCommand>
+    {
+        public Task Consume(ConsumeContext<IRegisterOrderCommand> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
