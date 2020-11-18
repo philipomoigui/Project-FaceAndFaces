364fb4a7560808f522e0f6aaa439f7b70f63b356
philip omoigui <philipomoigui@gmail.com>
2020-11-18 03:31:14 +0100

    Added Rabbitmq to handle communication

---
+++ Faces.SharedLib/Messaging.InterfacesConstant/Commands/IRegisterOrderCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messaging.InterfacesConstant.Commands
+{
+    public interface IRegisterOrderCommand
+    {
+        public string pictureUrl { get; set; }
+        public string UserEmail { get; set; }
+        public byte[] ImageData { get; set; }
+    }
+}
---
+++ Faces.SharedLib/Messaging.InterfacesConstant/Constants/RabbitMqMassTransitConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messaging.InterfacesConstant.Constants
+{
+    public class RabbitMqMassTransitConstants
+    {
+        public const string RabbitMqUri = "rabbitmq://rabbitmq:5672";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+        public const string RegisterOrderServiceQueue = "register.order.command";
+    }
+}
